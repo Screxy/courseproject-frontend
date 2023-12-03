06394ee57860a9237778ab846d846979eaff8e21
screxy <dvbvladis@mail.ru>
2023-12-03 23:58:57 +0300

    add table component

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {Table} from 'antd'
+import {ColumnsType} from 'antd/lib/table'
+interface DataType {
+  key: string;
+  name: string;
+  age: number;
+  address: string;
+}
+const dataSource: DataType[] = [
+  {
+    key: '1',
+    name: 'Леша',
+    age: 22,
+    address: '800-летия Москвы 28к1',
+  },
+  {
+    key: '2',
+    name: 'Миша',
+    age: 19,
+    address: 'Юбилейная 7к1',
+  },
+  {
+    key: '3',
+    name: 'Маша',
+    age: 18,
+    address: 'Николяна 612',
+  },
+  {
+    key: '4',
+    name: 'Кирилл',
+    age: 23,
+    address: 'Парк Победы 1',
+  },
+];
 
+const columns: ColumnsType<DataType> = [
+  {
+    title: 'Имя',
+    dataIndex: 'name',
+    key: 'name',
+  },
+  {
+    title: 'Возраст',
+    dataIndex: 'age',
+    key: 'age',
+  },
+  {
+    title: 'Адрес',
+    dataIndex: 'address',
+    key: 'address',
+  },
+];
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Table dataSource={dataSource} columns={columns} />
     </>
   )
 }
